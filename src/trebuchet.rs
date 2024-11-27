70a4f68ff99ee53d807115a97ecd5effd0f68bcd
Brandon Allen <brandon.allen@dexcom.com>
2024-11-27 14:11:04 -0800

    resolve calibrate argument as PathBuf

--- src/trebuchet.rs
+++ src/trebuchet.rs
@@ -1,4 +1,5 @@
 use clap::{Args, Subcommand};
+use std::path::PathBuf;
 
 #[derive(Args)]
 pub struct TrebuchetArgs {
@@ -13,14 +14,22 @@ enum Commands {
 
 #[derive(Debug, Args)]
 struct CalibrateArgs {
-    doc_path: Option<String>,
+    doc_path: Option<PathBuf>,
 }
 
 impl TrebuchetArgs {
     pub fn run(&self) -> String {
         match &self.command {
-            Some(Commands::Calibrate(args)) => format!("'calibrate' called with {:?}", args),
+            Some(Commands::Calibrate(args)) => {
+                let doc_path = args.doc_path.as_ref().unwrap();
+                let result = calibrate(doc_path);
+                format!("'calibrate' called with {:?}, result: {}", args, result)
+            }
             None => String::from("Trebuchet!"),
         }
     }
+}
+
+fn calibrate(_file: &PathBuf) -> i32 {
+    return 0;
 }
